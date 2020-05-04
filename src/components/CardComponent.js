acca78d2dd271c410a1b2871943c2188d0047c64
MUHAMMAD ILYAS <mail.ilyas@yahoo.com>
2020-05-04 23:53:16 +0500

    card component style updated

--- src/components/CardComponent.js
+++ src/components/CardComponent.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Image} from 'react-native';
+import {ImageBackground, StyleSheet} from 'react-native';
 import {
   Container,
   Content,
@@ -12,67 +12,96 @@ import {
   Left,
   Body,
 } from 'native-base';
-
+import {hp, wp} from '@UI/percentage';
 export default class CardComponent extends Component {
   render() {
     return (
-      <Container style={{ backgroundColor: "#030303" }}>
-        <Content>
-              <Body>
-                <Image
-                  source={require('../assets/images/ertugral.jpg')}
-                  style={{ borderRadius: 8, maxWidth: 400, maxHeight: 500   }}
-                />
-                <Text style={{ position: "absolute", paddingRight: 120, bottom: 100, color: "white", fontSize: 32, fontWeight: "normal"  }}>
-                  {/* Sultan Salahudeen Ayubi */}
-                  Drillis Ertugral
-                </Text>
-                <Text style={{ position: "absolute", paddingLeft: 30, paddingRight: 20, top: 410, color: "white", fontSize: 16, fontWeight: "bold"  }}>
-                  Sultan Salahudeen Ayubi. 1137 - 1193. 5 Volume PDF. This book
-                  tell us the true worior of islam. Kindly read this book and enjoy it.
-                  Sultan Salahudeen Ayubi. 1137 - 1193. 5 Volume PDF.
-                </Text>
-              </Body>
-              <CardItem style={{ bottom: -20, backgroundColor: "Transparent" }}>
-              <Left>
-                <Text style={{ fontWeight: "bold", fontSize: 20, color: "white" }}>
-                  Aslam Rahi
-                </Text>
-              </Left>
-              <Body>
-                <Text style={{ fontWeight: "bold", fontSize: 20, color: "white" }}>
-                  11-02-1998
-                </Text>
-              </Body>
-              <Right>
-                <Text style={{ fontWeight: "bold", fontSize: 20, color: "white" }}>
-                  34
-                </Text>
-              </Right>
-            </CardItem>
-            <CardItem style={{ backgroundColor: "Transparent", top: 10 }}>
-              <Left>
-                <Text style={{ fontSize: 10, color: "white" }}>
-                  Author
-                </Text>
-              </Left>
-              <Body>
-                <Text style={{ fontSize: 10, color: "white" }}>
-                  Publish Date
-                </Text>
-              </Body>
-              <Right>
-                <Text style={{ fontSize: 10, color: "white" }}>
-                  Total Volumes
-                </Text>
-              </Right>
-            </CardItem>
-            
-        </Content>
-        <Button block danger style={{ width: 300, bottom: 50, left: 30 }}>
-          <Text>Read Book</Text>
-        </Button>
+      <Container
+        style={{
+          backgroundColor: '#030303',
+        }}>
+        <ImageBackground
+          source={require('../assets/images/ertugral.jpg')}
+          style={{width: wp(100), height: hp(60)}}>
+          <View
+            style={{
+              position: 'absolute',
+              bottom: 0,
+              justifyContent: 'center',
+              alignItems: 'flex-start',
+              padding: wp(5),
+            }}>
+            <Text
+              numberOfLines={1}
+              style={{
+                color: 'white',
+                fontSize: wp(8),
+                fontWeight: 'normal',
+                fontWeight: 'bold',
+              }}>
+              Sultan Salahudeen Ayubi
+            </Text>
+            <Text
+              style={{
+                color: 'white',
+                fontSize: wp(4),
+                fontWeight: 'bold',
+              }}>
+              Sultan Salahudeen Ayubi. 1137 - 1193. 5 Volume PDF. This book tell
+              us the true worior of islam. Kindly read this book and enjoy it.
+              Sultan Salahudeen Ayubi. 1137 - 1193. 5 Volume PDF.
+            </Text>
+          </View>
+        </ImageBackground>
+        <View style={styles.tableWrapper}>
+          <View style={styles.table}>
+            <View style={styles.titleWrapper}>
+              <Text style={styles.title1}>Author</Text>
+              <Text style={styles.title2}>Aslam Rahi</Text>
+            </View>
+            <View style={styles.titleWrapper}>
+              <Text style={styles.title1}>Publish Date</Text>
+              <Text style={styles.title2}>11-FEb-98</Text>
+            </View>
+            <View style={styles.titleWrapper}>
+              <Text style={styles.title1}>Volumes</Text>
+              <Text style={styles.title2}>34</Text>
+            </View>
+          </View>
+
+          <Button block danger style={styles.readBookBt}>
+            <Text>Read Book</Text>
+          </Button>
+        </View>
       </Container>
     );
   }
 }
+const styles = StyleSheet.create({
+  table: {
+    flexDirection: 'row',
+    paddingVertical: hp(1),
+    justifyContent: 'space-evenly',
+  },
+  tableWrapper: {
+    flexDirection: 'column',
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  titleWrapper: {
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  title1: {
+    fontSize: wp(5),
+    color: 'white',
+  },
+  title2: {
+    fontSize: wp(3),
+    color: 'white',
+  },
+  readBookBt: {
+    width: wp(80),
+    alignSelf: 'center',
+  },
+});
